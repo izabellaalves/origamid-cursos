1ba905942947d194a6c2c3672433492d287b52fa
Izabella Alves <iap21010705@gmail.com>
2023-10-24 17:46:17 -0300

    chore: corrigindo estrutura de pastas

---
+++ javascript-completo-es6/javascript-para-iniciantes/tipos-de-dados/script.js
@@ -0,0 +1,23 @@
+// Declare uma variável contendo uma string
+var nome = 'iza';
+
+// Declare uma variável contendo um número dentro de uma string
+var ano = '21';
+
+// Declare uma variável com a sua idade
+var idade = 28;
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+var sobrenome = 'alves'
+var nomeCompleto = `${nome} ${sobrenome}`;
+
+// Coloque a seguinte frase em uma variável: It's time
+var frase = `It's time`;
+
+// Verifique o tipo da variável que contém o seu nome
+var verificarTipoNome = typeof nome;
+
+console.log(verificarTipoNome);
+
+// apagar
